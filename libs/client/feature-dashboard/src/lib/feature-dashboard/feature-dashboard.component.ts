c043c6ccc6bc21be32d29e4f2cba33626388d29a
Johannes Steudle <tweetjay2@icloud.com>
2023-07-18 14:43:41 +0200

    feat: use bike component in dashboard

--- libs/client/feature-dashboard/src/lib/feature-dashboard/feature-dashboard.component.ts
+++ libs/client/feature-dashboard/src/lib/feature-dashboard/feature-dashboard.component.ts
@@ -1,16 +1,52 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ApiService } from '@cpt/client/data-access';
+import { BikeComponent } from '@cpt/client/ui-components';
+import { Bike } from '@cpt/shared/domain';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'cycling-parts-tracker-feature-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BikeComponent],
   templateUrl: './feature-dashboard.component.html',
   styleUrls: ['./feature-dashboard.component.scss'],
 })
-export class FeatureDashboardComponent {
+export class FeatureDashboardComponent implements OnInit {
   private readonly apiService = inject(ApiService);
 
   bikeItems$ = this.apiService.getAllBikeItems();
+
+  trackTodo(idx: number, todo: Bike) {
+    return todo.id;
+  }
+
+  ngOnInit(): void {
+    this.refreshItems();
+  }
+
+  refreshItems() {
+    this.apiService
+      .getAllBikeItems()
+      .pipe(take(1))
+      .subscribe((items) => this.bikeItems$.next(items));
+  }
+
+  toggleComplete(todo: Bike) {
+    this.apiService
+      .updateBike(todo.id, { archived: !todo.archived })
+      .pipe(take(1))
+      .subscribe(() => {
+        this.refreshItems();
+      });
+  }
+
+  deleteTodo({ id }: Bike) {
+    this.apiService
+      .deleteBike(id)
+      .pipe(take(1))
+      .subscribe(() => {
+        this.refreshItems();
+      });
+  }
 }
