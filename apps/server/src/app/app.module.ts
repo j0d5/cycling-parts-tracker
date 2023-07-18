99de14fbbd756ece49c277e426aaae9a5a726d66
Johannes Steudle <tweetjay2@icloud.com>
2023-07-18 10:58:26 +0200

    feat: add database access basics

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
 
 import { ServerFeatureBikeModule } from '@cpt/server/feature-bike';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ServerFeatureBikeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        DATABASE_PATH: joi.string().default('tmp/db.sqlite'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get('DATABASE_PATH'),
+        synchronize: true,
+        logging: true,
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ServerFeatureBikeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
