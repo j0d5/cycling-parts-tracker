6d7172c97e3d690a7ea0430164eca1c9faadbee6
Johannes Steudle <tweetjay2@icloud.com>
2023-07-21 07:17:20 +0200

    refactor: change to mariadb

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,12 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 
 import { ServerFeatureBikeModule } from '@cpt/server/feature-bike';
 import { ServerFeatureHealthModule } from '@cpt/server/feature-health';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import joi from 'joi';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -17,19 +15,28 @@ import { AppService } from './app.service';
       }),
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({
-        type: 'sqlite',
-        database: config.get('DATABASE_PATH'),
-        synchronize: true,
-        logging: true,
-        autoLoadEntities: true,
-      }),
+      useFactory: (config: ConfigService) => {
+        const env = config.get('ENVIRONMENT') ?? 'development';
+        const dbType = config.getOrThrow('DATABASE_TYPE');
+        const dbName = config.getOrThrow('DATABASE_NAME');
+        Logger.debug(`Detected environment: ${env}`);
+        Logger.debug(`Attempting connection to ${dbType} database '${dbName}'`);
+        return {
+          type: dbType,
+          host: config.get('DATABASE_HOST'),
+          username: config.get('DATABASE_USERNAME'),
+          password: config.get('DATABASE_PASSWORD'),
+          port: config.get('DATABASE_PORT'),
+          database: dbName,
+          synchronize: true,
+          logging: true,
+          autoLoadEntities: true,
+        } as TypeOrmModuleAsyncOptions; // HERES THE PROBLEM
+      },
       inject: [ConfigService],
     }),
     ServerFeatureBikeModule,
     ServerFeatureHealthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
